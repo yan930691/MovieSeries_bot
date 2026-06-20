804682c0042de2e3c9e2a17ca97efa5ea90cc6c4
yan930691 <yan930691@gmail.com>
2026-06-21 06:37:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 MONGODB_URI = os.environ.get("MONGODB_URI")
-CHANNEL_ID = os.environ.get("CHANNEL_ID")  # None ထားလို့ရတယ်
+CHANNEL_ID = os.environ.get("CHANNEL_ID")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 0))
 RENDER_URL = os.environ.get("RENDER_URL")
 
