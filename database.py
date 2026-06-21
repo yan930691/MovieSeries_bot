bfb5986f15ee6fbd2ddd9a3b1384389b3849359e
yan930691 <yan930691@gmail.com>
2026-06-21 22:16:56 +0700

    Update database.py

--- database.py
+++ database.py
@@ -8,13 +8,6 @@ db = client[DB_NAME]
 posts_col = db["posts"]
 
 def save_post_data(poster_file_id, caption, seasons_data, telegraph_url):
-    """
-    Post Data ကို သိမ်းမယ်
-    seasons_data = {
-        "1": [{"text": "S1E1", "url": "https://..."}, ...],
-        "2": [{"text": "S2E1", "url": "https://..."}, ...]
-    }
-    """
     data = {
         "poster_file_id": poster_file_id,
         "caption": caption,
@@ -23,7 +16,3 @@ def save_post_data(poster_file_id, caption, seasons_data, telegraph_url):
         "created_at": datetime.utcnow()
     }
     return posts_col.insert_one(data)
-
-def get_latest_post():
-    """နောက်ဆုံး Post ကို ယူမယ် (အသုံးမပြုရသေးဘူး)"""
-    return posts_col.find_one(sort=[("_id", -1)])
