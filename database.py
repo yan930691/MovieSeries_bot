351da69ba221b5b8c42ae8c2af4dc3404490cbbd
yan930691 <yan930691@gmail.com>
2026-06-21 22:46:19 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,9 @@
 from pymongo import MongoClient
 from config import MONGODB_URI, DB_NAME
-from datetime import datetime
 
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
 
 posts_col = db["posts"]
 
-def save_post_data(poster_file_id, caption, seasons_data, telegraph_url):
-    data = {
-        "poster_file_id": poster_file_id,
-        "caption": caption,
-        "seasons": seasons_data,
-        "telegraph_url": telegraph_url,
-        "created_at": datetime.utcnow()
-    }
-    return posts_col.insert_one(data)
+# လိုအပ်ရင် နောက်မှ ထပ်ထည့်လို့ရတယ်
