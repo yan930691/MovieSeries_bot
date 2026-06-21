ca523bf6d458e110d142c8137960fc455889b72e
yan930691 <yan930691@gmail.com>
2026-06-22 01:34:36 +0700

    Update database.py

--- database.py
+++ database.py
@@ -5,10 +5,9 @@ from datetime import datetime
 client = MongoClient(MONGODB_URI, tls=True, tlsAllowInvalidCertificates=True)
 db = client[DB_NAME]
 
-files_col = db["files"]  # Deep Link အတွက် ဖိုင်တွေသိမ်းမယ်
+files_col = db["files"]
 
 def save_file_data(file_id, file_name, caption, deep_link, file_type):
-    """Deep Link အတွက် ဖိုင်ဒေတာကို သိမ်းမယ်"""
     data = {
         "file_id": file_id,
         "file_name": file_name,
@@ -20,5 +19,4 @@ def save_file_data(file_id, file_name, caption, deep_link, file_type):
     return files_col.insert_one(data)
 
 def get_file_by_deep_link(deep_link):
-    """Deep Link နဲ့ ဖိုင်ကို ရှာမယ်"""
     return files_col.find_one({"deep_link": deep_link})
