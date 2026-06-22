3ae3a46d9e94ad0b197aa7f67cb47e839b35e072
yan930691 <yan930691@gmail.com>
2026-06-22 08:20:15 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,17 +1,30 @@
+import os
 from pymongo import MongoClient
-from config import MONGODB_URI, DB_NAME
 from datetime import datetime
 
-client = MongoClient(MONGODB_URI)
-db = client[DB_NAME]
+# 🔥 Environment Variable ကနေ MONGO_URI ကိုဖတ်မယ်
+MONGO_URI = os.environ.get("MONGO_URI")
+if not MONGO_URI:
+    raise Exception("MONGO_URI not set in environment variables")
 
+# 🔥 MongoDB Connection
+client = MongoClient(
+    MONGO_URI,
+    tlsAllowInvalidCertificates=True,
+    serverSelectionTimeoutMS=3000,
+    connectTimeoutMS=3000,
+    socketTimeoutMS=3000
+)
+db = client["file_share_bot"]
+files_col = db["files"]
 posts_col = db["posts"]
 
-def save_post_data(poster_file_id, caption, seasons_data, telegraph_url):
+def save_post_data(poster_file_id, caption, seasons, telegraph_url):
+    """Post Data ကို သိမ်းမယ်"""
     data = {
         "poster_file_id": poster_file_id,
         "caption": caption,
-        "seasons": seasons_data,
+        "seasons": seasons,
         "telegraph_url": telegraph_url,
         "created_at": datetime.utcnow()
     }
